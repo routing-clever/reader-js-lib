8b3894f4a2f58e0de2e2fa5939d22994d3efa2b5
y-not-u <102951988+y-not-u@users.noreply.github.com>
2023-09-26 23:56:02 -0500

    fix: export email module

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import * as regexp from "./regexp";
 import * as string from "./string";
 import * as url from "./url";
 import * as html from "./html";
+import * as email from './email'
 
-export { regexp, string, url, html };
+export { regexp, string, url, html, email };
