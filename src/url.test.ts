61bb5c8899c304f8747286c240df309664ce8fda
y-not-u <102951988+y-not-u@users.noreply.github.com>
2024-08-28 22:04:30 +0800

    fix: url test

--- src/url.test.ts
+++ src/url.test.ts
@@ -6,16 +6,14 @@ describe("url module", () => {
   });
 
   it("should get the domain of an URL", () => {
-    expect(extractDomain("https://www.readzen.app")).toBe(
-      "https://www.readzen.app"
-    );
-    expect(extractDomain("http://www.readzen.app")).toBe(
-      "http://www.readzen.app"
-    );
-    expect(extractDomain("www.readzen.app")).toBe("https://www.readzen.app");
-    expect(extractDomain("img.readzen.app")).toBe("https://img.readzen.app");
-    expect(extractDomain("//www.readzen.app")).toBe("https://www.readzen.app");
-
+    expect(extractDomain('https://www.readzen.app')).toBe(
+      'https://www.readzen.app'
+    )
+    expect(extractDomain('http://www.readzen.app')).toBe(
+      'http://www.readzen.app'
+    )
+    expect(extractDomain('www.readzen.app')).toBe('https://www.readzen.app')
+    expect(extractDomain('img.readzen.app')).toBe('https://img.readzen.app')
     expect(extractDomain("file://www.readzen.app")).toBe(null);
   });
 });
